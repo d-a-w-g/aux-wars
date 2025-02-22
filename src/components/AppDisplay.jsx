5e21ce1bbc0305cc4089ae86a3262820b22386e5
Wilson Overfield <wkoverfield@gmail.com>
2025-02-22 16:51:36 -0600

    Implemented background display with animations.

---
+++ src/components/AppDisplay.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AlbumsDisplay from "./AlbumsDisplay";
+import { Outlet } from "react-router-dom";
+import albums from "./albums";
+
+export default function AppDisplay() {
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+      <AlbumsDisplay albums={albums} />
+      <div className="relative flex items-center justify-center h-full">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
