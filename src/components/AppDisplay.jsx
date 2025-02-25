71186c48de9d4d96dbbb1093303f057d8e902fa0
gracehecke <ghecke2@huskers.unl.edu>
2025-02-24 21:06:24 -0600

    added: join button, fixed: AppDisplay() overlap

--- src/components/AppDisplay.jsx
+++ src/components/AppDisplay.jsx
@@ -6,7 +6,9 @@ import albums from "./albums";
 export default function AppDisplay() {
   return (
     <div className="relative w-full h-screen overflow-hidden">
-      <AlbumsDisplay albums={albums} />
+      <div className="absolute top-0 left-0 right-0 bottom-0 z-0">
+        <AlbumsDisplay albums={albums} />
+      </div>
       <div className="relative flex items-center justify-center h-full">
         <Outlet />
       </div>
--- src/features/login/Login.jsx
+++ src/features/login/Login.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <button 
+      className="button">
+      Join game
+    </button>
+  );
 }
