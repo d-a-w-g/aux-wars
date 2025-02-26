7b2c4980eaa665bb88039b0155763addb386a284
gracehecke <ghecke2@huskers.unl.edu>
2025-02-25 18:12:59 -0600

    added: logo, host a game

--- src/features/login/Login.jsx
+++ src/features/login/Login.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./login.css"
+import logo from "./landing-logo.png";
 
 export default function Login() {
   return (
-    <div>
+    <div className="container">
+      <img src={logo} alt="Game Logo" className="logo" />
       <input className="text-field" type="text" placeholder="Enter Code"/>
-      <button className="button">Join game</button>
+      <button className="join-game-button">Join game</button>
+      <button className="host-a-game-button">Host a game</button>
     </div>
   );
 }
