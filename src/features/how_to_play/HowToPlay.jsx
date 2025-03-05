d9ce9b6b2168143b74a09453e2f5a188ab716493
gracehecke <ghecke2@huskers.unl.edu>
2025-03-04 18:01:29 -0600

    fixed: rules display
    still questionable ngl

--- src/features/how_to_play/HowToPlay.jsx
+++ src/features/how_to_play/HowToPlay.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './how_to_play.css';
-import logo from './landing-logo.png'; 
+import logo from './landing-logo.png';
 
 export default function HowToPlay() {
   return (
-    <div className="container">
-      <div className="text-box">
-        <img src={logo} alt="Game Logo" className="logo" />
-        <h2>How to Play:</h2>
-        <ol className="rule-body">
+    <div className="rules-background">
+        <img src={logo} alt="Game Logo" className="rules-logo" />
+        <h2  className="rules-title">How to Play:</h2>
+        <ol className="rules-body">
           <li>1. Join a lobby with friends using the game code provided by the host.</li>
           <li>2. Each round, you’ll receive a prompt and have a set amount of time to choose a song, selecting the section that best fits the prompt.</li>
           <li>3. Once all songs are submitted, players will rate each entry on a scale of 1 to 5 records.</li>
@@ -16,15 +15,14 @@ export default function HowToPlay() {
           <li>5. At the end of the game, the player with the most records wins!</li>
         </ol>
         
-        <h2>Rules:</h2>
-        <ol className="rule-body">
-          <li>1. Rule.</li>
-          <li>2. This is a rule.</li>
-          <li>3. More rules.</li>
-          <li>4. Anotha one.</li>
-          <li>5. Yassss</li>
+        <h2 className="rules-title">Rules:</h2>
+        <ol className="rules-body">
+        <li>1. Replace this with rules.</li>
+          <li>2. Rules rules rules.</li>
+          <li>3. Once all songs are submitted, players will rate each entry on a scale of 1 to 5 records.</li>
+          <li>4. Repeat until all rounds are done.</li>
+          <li>5. At the end of the game, the player with the most records wins!</li>
         </ol>
-      </div>
       
       <button className="back-to-login-button">Back to login</button>
     </div>
