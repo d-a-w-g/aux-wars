da8285f654fcbae68387f4170bd09514c33faf81
gracehecke <ghecke2@huskers.unl.edu>
2025-03-30 23:17:27 -0500

    Added: Next button

--- src/features/round-winner/RoundWinner.jsx
+++ src/features/round-winner/RoundWinner.jsx
@@ -6,7 +6,12 @@ export default function RoundWinner({songs}) {
     
     return (
       <div className="round-winner"> 
+      <div className="top-bar">
+      <div className="prdisplayompt-">
         <PromptDisplay />
+        </div>
+        <button className="next-button">Next</button>
+      </div>
         <div className="all-songs">
           {songs.map((song, index) => (
             <div key={index}>
