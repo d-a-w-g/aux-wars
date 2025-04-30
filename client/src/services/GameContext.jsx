bdebbb32d38ebc30a30929f93839d5de431e80ec
Wilson Overfield <wkoverfield@gmail.com>
2025-04-30 14:14:38 -0500

    quick settings modal fix

--- client/src/services/GameContext.jsx
+++ client/src/services/GameContext.jsx
@@ -7,6 +7,32 @@ const initialState = {
   players: [],
   phase: 'lobby',
   gameCode: '',
+  availablePrompts: [
+    "A song that makes you feel nostalgic",
+    "A song that reminds you of summer",
+    "A song that gets you pumped up",
+    "A song that makes you want to dance",
+    "A song that you love but others might not know",
+    "A song that tells a great story",
+    "A song that you discovered recently",
+    "A song that you associate with a specific memory",
+    "A song that you think is underrated",
+    "A song that you could listen to on repeat"
+  ],
+  selectedPrompts: [
+    "A song that makes you feel nostalgic",
+    "A song that reminds you of summer",
+    "A song that gets you pumped up",
+    "A song that makes you want to dance",
+    "A song that you love but others might not know",
+    "A song that tells a great story",
+    "A song that you discovered recently",
+    "A song that you associate with a specific memory",
+    "A song that you think is underrated",
+    "A song that you could listen to on repeat"
+  ],
+  numberOfRounds: 3,
+  roundLength: 30,
 };
 
 const gameReducer = (state, action) => {
@@ -19,6 +45,14 @@ const gameReducer = (state, action) => {
       return { ...state, phase: action.payload };
     case 'SET_GAME_CODE':
       return { ...state, gameCode: action.payload };
+    case 'SET_AVAILABLE_PROMPTS':
+      return { ...state, availablePrompts: action.payload };
+    case 'SET_SELECTED_PROMPTS':
+      return { ...state, selectedPrompts: action.payload };
+    case 'SET_ROUNDS':
+      return { ...state, numberOfRounds: action.payload };
+    case 'SET_ROUND_LENGTH':
+      return { ...state, roundLength: action.payload };
     default:
       return state;
   }
